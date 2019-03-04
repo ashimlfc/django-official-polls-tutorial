bc33a40955d2aec690ed17c0cfd77f005548ce24
Ashim Shrestha <ashimshrestha840@gmail.com>
2019-03-04 21:55:26 +0545

    vote option, vote form and buttons added

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from django.urls import reverse
 
 from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -22,7 +24,20 @@ def results(request, question_id):
 	return HttpResponse("These are the results of the question: %s" % question_id)
 
 def vote(request, question_id):
-	return HttpResponse("Vote on Question: %s" % question_id)
+	question = get_object_or_404(Question, pk = question_id)
+	try:
+		selected_choice = question.choice_set.get(pk = request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
 
+		#Redisplaying the voting form
+		return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice."})
 
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+
+		#returning an HttpResponse Redirect after succesfully dealig with POST data
+		#prevents data from being posted twice if a user hits the back button
+
+	return HttpResponseRedirect(reverse('polls:results', args = (question_id,)))
  
